3526486e815048abbedb021338789b0651134b6e
nqvuong99qn <vuong.nguyen.quang@tpssoft.com>
2020-09-08 17:26:58 +0700

    initial code

---
+++ BND/src/main/java/com/BitcoinNevaDie/BND/BndApplication.java
@@ -0,0 +1,13 @@
+package com.BitcoinNevaDie.BND;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BndApplication.class, args);
+	}
+
+}
---
+++ BND/src/test/java/com/BitcoinNevaDie/BND/BndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BitcoinNevaDie.BND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
